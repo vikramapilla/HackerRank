3d0f935390cc8191042bb9a0aff23032444e8e5c
Vikram Apilla <vikramapilla@gmail.com>
2017-06-01 10:43:34 +0530

    Create SolveMeFirst.java

---
+++ Algorithms/Warmup/SolveMeFirst.java
@@ -0,0 +1,36 @@
+
+/**
+ * HackerRank/Algorithms/Warmup
+ * SolveMeFirst.java
+ * Purpose: Prints the sum of two numbers to the screen.
+ *
+ * @author Vikram Apilla
+ * @version 1.0 11/01/16
+ */
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+   static int solveMeFirst(int a, int b) {
+      return a+b; // Returns the sum of a & b
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
+}
