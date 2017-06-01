5a950c30e8c44c895b45e1167a60672597f619f7
Vikram Apilla <vikramapilla@gmail.com>
2017-06-01 19:48:44 +0530

    Create Bash.sh

---
+++ Linux Shell/Bash.sh
@@ -0,0 +1,91 @@
+# 1. Let's Echo
+
+echo "HELLO"
+
+# 2. Looping and Skipping
+
+for i in {1..100..2}
+do
+    echo $i
+done
+
+# 3. A Personalized Echo
+
+read name
+echo Welcome $name
+
+# 4. Looping with Numbers
+
+for i in {1..50}
+do
+    echo $i
+done
+
+# 5. The World of Numbers
+
+read x
+read y
+echo $[x + y]
+echo $[x - y]
+echo $[x * y]
+echo $[x / y]
+
+# 6. Comparing Numbers
+
+read x
+read y
+if [ $x -gt $y ]
+then
+    echo X is greater than Y 
+elif [ $x -lt $y ]
+then
+    echo X is less than Y
+else
+    echo X is equal to Y
+fi
+
+# 7. Getting started with conditionals
+
+read c
+if [ $c == y ] || [ $c == Y ]
+then
+echo YES
+elif [ $c == n ] || [ $c == N ]
+then
+echo NO
+fi
+
+# 8. More on Conditionals
+
+read a
+read b
+read c
+
+if [ $a -eq $b ] && [ $b -eq $c ]
+then
+    echo EQUILATERAL
+elif [ $a -ne $b ] && [ $b -ne $c ] && [ $a -ne $c ]
+then
+    echo SCALENE
+else
+    echo ISOSCELES
+fi
+
+# 9. Arithmetic Operations
+
+read exp
+printf "%.3f\n" $(echo "scale=4; $exp" | bc)
+
+# 10. Compute the Average
+
+read n
+sum=0
+for (( i=0; i<n; i++ ))
+do
+   read x
+   sum=$[sum+x]
+done
+
+printf "%.3f" $(echo $sum/$n | bc -l)
+
+
